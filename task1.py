1e5eb3c7d38a28d8946758895a2dbda7b8b75256
Timi Salminen <timi_salminen@hotmail.com>
2022-10-26 20:39:45 +0300

    ekat 10%

--- task1.py
+++ task1.py
@@ -1,12 +1,14 @@
-# open text
-
 import csv
+
+# open ratings.csv file and read all the lines to data list
 
 with open('ratings.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
-    
-print(data)
 
-# print some text lines + line count
+# print first 4 rows (column names + 3 first value rows)
+# print list length
 
+for x in range(4):
+    print(data[x])
+print(len(data))
