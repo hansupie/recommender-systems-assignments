e9e95476c89975109227140c54ebd6e9b6850a3e
Timi Salminen <timi_salminen@hotmail.com>
2022-10-26 22:26:58 +0300

    muutettu lista listaksi dictejä

--- task1.py
+++ task1.py
@@ -1,14 +1,15 @@
-import csv
+from csv import DictReader
 
 # open ratings.csv file and read all the lines to data list
 
-with open('ratings.csv', newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+with open("ratings.csv", 'r') as f:
+    dict_reader = DictReader(f)
+    list_of_dict = list(dict_reader)
 
 # print first 4 rows (column names + 3 first value rows)
 # print list length
 
 for x in range(4):
-    print(data[x])
-print(len(data))
+    print(list_of_dict[x])
+print(len(list_of_dict))
+
