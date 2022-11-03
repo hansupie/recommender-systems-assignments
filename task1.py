72c633259ac6930adf7e81d5fca819ba129684db
Hannemari Kuusisto <hannemari.kuusisto@tuni.fi>
2022-11-03 22:50:02 +0200

    ei ihan toimi vielä

--- task1.py
+++ task1.py
@@ -2,9 +2,6 @@ from pandas.core.frame import DataFrame
 from itertools import groupby
 import pandas as pd
 import numpy as np
-
-'''   This part was used to calculate corrcoef, but since it took so long, we wrote it to
-      to calculated_corr.csv and now just load the csv :D
       
 # open ratings.csv file and read all the lines to data list
 r_data=pd.read_csv('ratings.csv',sep=',',header='infer',quotechar='\"')
@@ -26,12 +23,16 @@ data = r_data.merge(m_data,on="movieId")
 data.drop(['timestamp'],inplace=True,axis=1)
 
 data_table = pd.pivot_table(data,values='rating',columns='userId',index='title')
-#print('MOVIELENS')
-# print(data_table.head(10))
+
+print('MOVIELENS')
+print(data_table.head(10))
 
 # just testing with smaller data
-#data_table = pd.read_csv('small_movie_ratings.csv', index_col=0)
-#print(data_table)
+# data_table = pd.read_csv('small_movie_ratings.csv', index_col=0)
+# print(data_table)
+
+'''   This part was used to calculate corrcoef, but since it took so long, we wrote it to
+      to calculated_corr.csv and now just load the csv :D
 
 # function to check if all values are equal
 def all_equal(iterable):
@@ -71,7 +72,7 @@ calc_data.index = np.arange(1, len(calc_data) + 1)
 print(calc_data)
 
 
-# nearest 10 correlations for user '1'
+#nearest 10 correlations for user '1'
 
 # sorts list to nearest ones to user '1'
 nearest = calc_data.sort_values(axis = 'index', by='1', ascending=False)
@@ -79,3 +80,76 @@ nearest = calc_data.sort_values(axis = 'index', by='1', ascending=False)
 # removes all other colums except '1'
 nearest = nearest[['1']]
 print(nearest.head(11))
+
+
+# predict movie scores
+
+# function to select only users who have rated the movie
+def get_users_who_rated(data_table: pd.DataFrame, movie: str):
+  return data_table.loc[movie, :].dropna().index.values
+
+# get n top neighbours for user
+# returns a dictionary of users and their similarity to user
+def get_top_neighbours(similarity_data: pd.DataFrame, user: str, users_who_rated: str, n: int):
+  return similarity_data[user][users_who_rated].nlargest(n).to_dict()
+
+# adjust for bias
+
+# subtract user's mean rating from a rating
+# returns adjusted rating
+def substract_bias(rating: float, mean_rating: float):
+  return rating - mean_rating
+
+def get_rating_without_bias(data_table: pd.DataFrame, user: str, movie: str):
+  mean_rating = data_table[int(user)].mean()
+  rating = data_table.loc[movie, int(user)]
+  return substract_bias(rating, mean_rating)
+
+def get_ratings_of_neighbours(data_table: pd.DataFrame, neighbours: list, movie: str):
+  return [
+    get_rating_without_bias(data_table, neighbour, movie)
+    for neighbour in neighbours
+  ]
+
+# predicting user rating for a movie
+# first get weighted average of neighbours' ratings
+# then get the rating by adjusting for bias
+
+# dividing by 0 :)
+def get_weighted_average_rating_of_neighbours(ratings: list, neighbour_dist: list):
+  weighted_sum = np.array(ratings).dot(np.array(neighbour_dist))
+  abs_neighbour_dist = np.abs(neighbour_dist)
+  # if np.sum(abs_neighbour_dist) == 0:
+  #   print("neighbour_dist", neighbour_dist)
+  #   print(np.sum(abs_neighbour_dist))
+  return weighted_sum / np.sum(abs_neighbour_dist)
+
+def get_user_rating(data_table: pd.DataFrame, user: str, avg_neighbour_rating: float):
+  user_avg_rating = data_table[int(user)].mean()
+  return round(user_avg_rating + avg_neighbour_rating, 2)
+
+def predict_rating(data: pd.DataFrame, similarity_data: pd.DataFrame, user: int, movie: str, neighbours: int = 30):
+  data_table = data.copy()
+  rated_users = get_users_who_rated(data_table, movie)
+  top_neighbours_dist = get_top_neighbours(similarity_data, str(user), rated_users, neighbours)
+  neighbours, distance = top_neighbours_dist.keys(), top_neighbours_dist.values()
+  ratings = get_ratings_of_neighbours(data_table, neighbours, movie)
+  avg_neighbour_rating = get_weighted_average_rating_of_neighbours(ratings, list(distance))
+  return get_user_rating(data_table, user, avg_neighbour_rating)
+
+all_ratings = data_table.copy()
+selected_user = 1
+
+user_ratings = []
+for movie in data_table.index:
+  if np.isnan(all_ratings.loc[movie, selected_user]):
+    rating = predict_rating(data_table, calc_data, selected_user, movie)
+    user_ratings.append(rating)
+
+rating_list = list(zip(data_table.index, user_ratings))
+
+rating_df = pd.DataFrame(rating_list, columns=['Movie', 'Rating'])
+print(rating_df)
+
+movie_recs = rating_df.sort_values('Rating', ascending=False)
+print(movie_recs.head(20))
